b5f35220ba479bf9fe6701c7aeb173129ccaa17f
Roy Lu <royvbtw@gmail.com>
2017-03-21 21:53:35 +0800

    add &! to echo statement

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -1,6 +1,6 @@
-# deploy/after-install
+# deploy/after-install.sh
 #!/bin/bash
 npm install
 nohup node /var/node/myhome/index.js > /dev/null 2> /dev/null < /dev/null &
-echo > ~/myhome.pid
+echo $! > ~/myhome.pid
 
