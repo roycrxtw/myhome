82a032221debdc964c1c65149cc8bca0dcbfd44e
Roy Lu <royvbtw@gmail.com>
2017-03-22 11:25:09 +0800

    add source setting

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -1,4 +1,8 @@
 # deploy/after-install.sh
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 npm install
+sudo chown -R ec2-user /var/node/myhome
+sudo chgrp -R ec2-user /var/node/myhome
+chmod -R 755 /var/node/myhome
 pm2 start /var/node/myhome/index.js --name='myhome'
