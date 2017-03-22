d2b746eab523d29f0f735ccb48f0be8d7d8bc8b2
Roy Lu <royvbtw@gmail.com>
2017-03-22 22:08:26 +0800

    uncomment pm2 start instruction

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -6,4 +6,4 @@ sudo chown -R ec2-user /var/node/myhome
 sudo chgrp -R ec2-user /var/node/myhome
 chmod -R 755 /var/node/myhome
 cd /var/node/myhome
-#pm2 start /var/node/myhome/index.js --name='myhome'
+pm2 start index.js --name='myhome'
