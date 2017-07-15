b5811a7e51454d7c5aeb5e0e3951fd075318fafe
Roy Lu <royvbtw@gmail.com>
2017-07-16 03:17:07 +0800

    Move the project to nginx
    Move the project from nodegs to nginx since this
    project only needs static html pages.

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -1,9 +1,7 @@
 # deploy/after-install.sh
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-npm install
-sudo chown -R ec2-user /var/node/myhome
-sudo chgrp -R ec2-user /var/node/myhome
-chmod -R 755 /var/node/myhome
-cd /var/node/myhome
-pm2 start index.js --name='myhome'
+cd /usr/share/nginx
+sudo chown -R ec2-user /usr/share/nginx
+sudo chgrp -R ec2-user /usr/share/nginx
+chmod -R 755 /usr/share/nginx
--- deploy/before-install.sh
+++ deploy/before-install.sh
@@ -1,9 +1,6 @@
 # deploy/before-install
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-pm2 delete myhome
-cp /var/node/myhome/config ~/backup/myhome/
 shopt -s extglob
-rm -fr /var/node/myhome/.gitignore
-rm -fr /var/node/myhome/!(config|node_modules)
+rm -fr /usr/share/nginx
 shopt -u extglob
